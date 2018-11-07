87ab21cf1d97bd3d6cc2837115a549d074d5f07b
= <jo123.garcia.s@gmail.com>
2018-11-06 22:00:26 -0600

    up config

--- src/config/omdb.js
+++ src/config/omdb.js
@@ -1,10 +1,1 @@
-/* --- Enviroment --- */
-let API_KEY = null;
-
-if (process.env.NODE_ENV === 'development') {
-  API_KEY= '3aaf6bc6';
-} else {
-  API_KEY = process.env.OMDB_KEY;
-}
-
-export const OMDB_API_KEY = API_KEY;
+export const OMDB_API_KEY = '3aaf6bc6';
