c9d50bfa8598b56b51a1340830565932d68b51b7
= <jo123.garcia.s@gmail.com>
2018-11-06 19:51:44 -0600

    Updates

---
+++ precache-manifest.549c7f04d85b19d4ffcd9cac49612d12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "382a497d6590e166542c64dea3b406d4",
+    "url": "/Georgge/react-search-movie-exercise/static/media/bg.382a497d.jpeg"
+  },
+  {
+    "revision": "b371d367daa9d8d135c5",
+    "url": "/Georgge/react-search-movie-exercise/static/js/runtime~main.b371d367.js"
+  },
+  {
+    "revision": "eb5a75d7027cca572582",
+    "url": "/Georgge/react-search-movie-exercise/static/js/main.eb5a75d7.chunk.js"
+  },
+  {
+    "revision": "f7b96c18274c69a343e8",
+    "url": "/Georgge/react-search-movie-exercise/static/js/1.f7b96c18.chunk.js"
+  },
+  {
+    "revision": "eb5a75d7027cca572582",
+    "url": "/Georgge/react-search-movie-exercise/static/css/main.97887052.chunk.css"
+  },
+  {
+    "revision": "f7b96c18274c69a343e8",
+    "url": "/Georgge/react-search-movie-exercise/static/css/1.74ea84d2.chunk.css"
+  },
+  {
+    "revision": "8fa5493838f6a7dc82928301ca9a7007",
+    "url": "/Georgge/react-search-movie-exercise/index.html"
+  }
+];
